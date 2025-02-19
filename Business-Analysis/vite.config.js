8fe79730479bfd3ed4b0e893ed93ae396ab6d0cf
SOURABH <sourabhrm25@gmail.com>
2025-02-19 23:03:51 +0530

    react-dom-install

--- Business-Analysis/vite.config.js
+++ Business-Analysis/vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-    ,tailwindcss()
+  plugins: [react(),
+    tailwindcss()
   ],
- 
+
 })
